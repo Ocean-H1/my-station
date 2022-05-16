4325fd47a6d26668d81ef45450223a589afd35ce
OceanH <1404939185@qq.com>
2022-05-17 00:20:06 +0800

    完成searchTicket中点击最后一个tab项加载更多日期的功能

--- src/views/purchase/components/searchTicket.vue
+++ src/views/purchase/components/searchTicket.vue
@@ -1,11 +1,37 @@
 <template>
   <div class="searchTicket">
     <!-- tabs部分 卡片化 -->
-    <el-tabs type="border-card">
-      <el-tab-pane :label="getCurrentDate(0)">111</el-tab-pane>
-      <el-tab-pane :label="getCurrentDate(1)">配置管理</el-tab-pane>
-      <el-tab-pane :label="getCurrentDate(2)">角色管理</el-tab-pane>
-      <el-tab-pane :label="getCurrentDate(3)">定时任务补偿</el-tab-pane>
+    <el-tabs type="border-card" v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane :name="getCurrentDate(0)">
+        <span slot="label"
+          ><i class="el-icon-date"></i> {{ getCurrentDate(0) }}</span
+        >
+        1
+      </el-tab-pane>
+      <el-tab-pane :name="getCurrentDate(1)">
+        <span slot="label"
+          ><i class="el-icon-date"></i> {{ getCurrentDate(1) }}</span
+        >
+        2
+      </el-tab-pane>
+      <el-tab-pane :name="getCurrentDate(2)">
+        <span slot="label"
+          ><i class="el-icon-date"></i> {{ getCurrentDate(2) }}</span
+        >
+        3
+      </el-tab-pane>
+      <el-tab-pane :name="getCurrentDate(3)">
+        <span slot="label"
+          ><i class="el-icon-date"></i> {{ getCurrentDate(3) }}</span
+        >
+        4
+      </el-tab-pane>
+      <el-tab-pane :name="getCurrentDate(4)">
+        <span slot="label" @click="addDays(2)"
+          ><i class="el-icon-date"></i> {{ getCurrentDate(4) }}</span
+        >
+        5
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -14,12 +40,24 @@
 export default {
   name: 'searchTicket',
   data() {
-    return {}
+    return {
+      startDate: this.$moment(),
+      activeName: this.$moment().format('MM-DD')
+    }
+  },
+  methods: {
+    addDays(num) {
+      this.startDate = this.$moment(this.startDate).add(num, 'days')
+    },
+    // 处理选项卡切换事件
+    handleClick(tab,event) {
+        console.log(tab.name);
+    }
   },
   computed: {
     getCurrentDate() {
       return function (num) {
-        return this.$moment().add(num, 'days').format('MM月DD日')
+        return this.$moment(this.startDate).add(num, 'days').format('MM-DD')
       }
     },
   },
